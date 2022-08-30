f3b8275402cddb4820047c8da4864ad478fb56b5
我有点帅哦 <1543995180@qq.com>
2022-08-30 17:53:47 +0800

    auto update

--- kernel/add2_kernel.cu
+++ kernel/add2_kernel.cu
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<iostream>
 
 __global__ void add2_kernel(float* c,
                             const float* a,
@@ -18,5 +19,6 @@ void launch_add2(float* c,
     dim3 grid((n + 1023) / 1024);
     dim3 block(1024);
     add2_kernel<<<grid, block>>>(c, a, b, n);
+
    
 }
